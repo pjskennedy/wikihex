4be2fafe4cf52d8488b6402b58aec0e21e704d6f
pjskennedy <peterjskennedy@gmail.com>
2018-11-07 19:38:35 -0800

    Created empty project

---
+++ lib/wikihex.ex
@@ -0,0 +1,18 @@
+defmodule Wikihex do
+  @moduledoc """
+  Documentation for Wikihex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wikihex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
